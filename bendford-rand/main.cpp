2910a457f445f7464a3117dfc6c6765faf36679f
Greg Gascon <greg.gascon@gmail.com>
2018-05-12 16:43:33 -0300

    Initial Commit

---
+++ bendford-rand/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bendford-rand
+//
+//  Created by Gregory Gascon on 2018-05-12.
+//  Copyright © 2018 ggascon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
