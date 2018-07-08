ca8f7ab2b306ae0eb0e2a8c2586462a8a777d669
Greg Gascon <greg.gascon@gmail.com>
2018-07-08 13:46:17 -0300

    Add ability to determine probability of numbers past first digit

--- benford-rand/benford_rand.cpp
+++ benford-rand/benford_rand.cpp
@@ -35,7 +35,7 @@ class benford_rand{
             double probability = 0;
             if (digit == 1){
                 if (n == 0.0){
-                    probability = 0.12;
+                    probability = 0;
                 }
                 else{
                     probability = log10(1+(1/n)); //Benford's law probability rule
@@ -57,15 +57,15 @@ class benford_rand{
     
     public:
         int rand(int pool_size = 100000){
-            unsigned int arr_size = 0;
+            unsigned int first_digit_arr_size = 0;
             
             //Determine size of random number pool for the first digit
             for (int i = 0; i < 10; i++){
                 double probability = get_probability(i*1.0);
-                arr_size += (int)((probability*pool_size)+0.5);
+                first_digit_arr_size += (int)((probability*pool_size)+0.5);
             }
             
-            int first_digit_pool[arr_size];
+            int first_digit_pool[first_digit_arr_size];
             int arr_counter = 0;
             //Creates pool of numbers following Benford's law to pull randomly from
             for (int i = 0 ; i < 10; i++){
@@ -76,25 +76,103 @@ class benford_rand{
                     arr_counter+=1;
                 }
             }
-            std::random_shuffle(&first_digit_pool[0], &first_digit_pool[arr_size]);
+            std::random_shuffle(&first_digit_pool[0], &first_digit_pool[first_digit_arr_size]);
             
             
+            //Determine size of second/third digit random number pool
+            unsigned int second_digit_arr_size = 0;
+            for (int i = 0; i < 10; i++){
+                double probability = get_probability(i*1.0, 2);
+                second_digit_arr_size += (int)((probability*pool_size)+0.5);
+            }
             
-
+            int second_digit_pool[second_digit_arr_size];
+            arr_counter = 0;
+            //Creates pool of numbers following Benford's law to pull randomly from
+            for (int i = 0 ; i < 10; i++){
+                double probability = get_probability(i*1.0, 2);
+                int n_norm_probability = (int)((probability*pool_size)+0.5);
+                for (int p = 0; p < n_norm_probability; p++){
+                    second_digit_pool[arr_counter] = i;
+                    arr_counter+=1;
+                }
+            }
+            std::random_shuffle(&second_digit_pool[0], &second_digit_pool[second_digit_arr_size]);
             
+            
+            //Determine size of second/third digit random number pool
+            unsigned int third_digit_arr_size = 0;
+            for (int i = 0; i < 10; i++){
+                double probability = get_probability(i*1.0, 3);
+                third_digit_arr_size += (int)((probability*pool_size)+0.5);
+            }
+            
+            int third_digit_pool[third_digit_arr_size];
+            arr_counter = 0;
+            //Creates pool of numbers following Benford's law to pull randomly from
+            for (int i = 0 ; i < 10; i++){
+                double probability = get_probability(i*1.0, 3);
+                int n_norm_probability = (int)((probability*pool_size)+0.5);
+                for (int p = 0; p < n_norm_probability; p++){
+                    third_digit_pool[arr_counter] = i;
+                    arr_counter+=1;
+                }
+            }
+            std::random_shuffle(&third_digit_pool[0], &third_digit_pool[third_digit_arr_size]);
+            
+            //Determine size of second/third digit random number pool
+            unsigned int fourth_digit_arr_size = 0;
+            for (int i = 0; i < 10; i++){
+                double probability = get_probability(i*1.0, 4);
+                fourth_digit_arr_size += (int)((probability*pool_size)+0.5);
+            }
+            
+            int fourth_digit_pool[fourth_digit_arr_size];
+            arr_counter = 0;
+            //Creates pool of numbers following Benford's law to pull randomly from
+            for (int i = 0 ; i < 10; i++){
+                double probability = get_probability(i*1.0, 4);
+                int n_norm_probability = (int)((probability*pool_size)+0.5);
+                for (int p = 0; p < n_norm_probability; p++){
+                    fourth_digit_pool[arr_counter] = i;
+                    arr_counter+=1;
+                }
+            }
+            std::random_shuffle(&fourth_digit_pool[0], &fourth_digit_pool[fourth_digit_arr_size]);
             
             int rand_size_min = 1;
             int rand_size_max = 9;
             int rand_size = time_rand(rand_size_min,rand_size_max); //Determines number of digits within random number
             std::string rand_str;
             for (int i = 0; i < rand_size;i++){
-                int rand_index_min = 1;
-                int rand_index_max = arr_size;
-                int rand_index = time_rand(rand_index_min,rand_index_max);
-                rand_str += std::to_string(first_digit_pool[rand_index]);
+                if (i == 0){
+                    int rand_index_min = 1;
+                    int rand_index_max = first_digit_arr_size;
+                    int rand_index = time_rand(rand_index_min,rand_index_max);
+                    rand_str += std::to_string(first_digit_pool[rand_index]);
+                }
+                else if (i == 1){
+                    int rand_index_min = 1;
+                    int rand_index_max = second_digit_arr_size;
+                    int rand_index = time_rand(rand_index_min,rand_index_max);
+                    rand_str += std::to_string(second_digit_pool[rand_index]);
+                }
+                else if (i == 2){
+                    int rand_index_min = 1;
+                    int rand_index_max = third_digit_arr_size;
+                    int rand_index = time_rand(rand_index_min,rand_index_max);
+                    rand_str += std::to_string(third_digit_pool[rand_index]);
+                }
+                else {
+                    int rand_index_min = 1;
+                    int rand_index_max = fourth_digit_arr_size;
+                    int rand_index = time_rand(rand_index_min,rand_index_max);
+                    rand_str += std::to_string(fourth_digit_pool[rand_index]);
+                }
+                
             }
-            //std::cout << rand_str << std::endl;
-            int rand = std::stoi(rand_str);
+            std::cout << rand_str << std::endl;
+            int rand = std::stol(rand_str);
             return rand;
         }
 };
