794efeae7c4a4ecd5d850a6cc70c857dee7c3b9e
Greg Gascon <greg.gascon@gmail.com>
2018-07-08 13:17:25 -0300

    Add ability to generate probabilities for digits past first digit

--- benford-rand/benford_rand.cpp
+++ benford-rand/benford_rand.cpp
@@ -30,13 +30,28 @@ class benford_rand{
             return min + (nanos) % (int)(max - min + 1);
         }
     
-        double get_probability(double n){
+        double get_probability(double n, int digit=1){
             //Probability a number will exist based on Bendford's law
-            if (n == 0.0){
-                return 0.12;
+            double probability = 0;
+            if (digit == 1){
+                if (n == 0.0){
+                    probability = 0.12;
+                }
+                else{
+                    probability = log10(1+(1/n)); //Benford's law probability rule
+                }
             }
-            double probability;
-            probability = log10(1+(1/n));
+            else if (digit < 4){ //if digit is 2-3 use Benford's law generalized past 1 digit
+                double max_value = pow(10,(digit-1))-1;
+                double k = 0;
+                for (int i = 0; i < max_value; i++){
+                    k += pow(10,(digit-2));
+                    probability += log10(1+(1/((10*k)+n)));
+                }
+            }
+            else{ //Digits greater than four have probability of 10% for each number
+                probability = 0.1;
+            }
             return probability;
         }
     
@@ -44,34 +59,41 @@ class benford_rand{
         int rand(int pool_size = 100000){
             unsigned int arr_size = 0;
             
+            //Determine size of random number pool for the first digit
             for (int i = 0; i < 10; i++){
                 double probability = get_probability(i*1.0);
                 arr_size += (int)((probability*pool_size)+0.5);
-                
             }
             
-            int array[arr_size];
+            int first_digit_pool[arr_size];
             int arr_counter = 0;
+            //Creates pool of numbers following Benford's law to pull randomly from
             for (int i = 0 ; i < 10; i++){
                 double probability = get_probability(i*1.0);
                 int n_norm_probability = (int)((probability*pool_size)+0.5);
                 for (int p = 0; p < n_norm_probability; p++){
-                    array[arr_counter] = i;
+                    first_digit_pool[arr_counter] = i;
                     arr_counter+=1;
                 }
             }
-            std::random_shuffle(&array[0],&array[arr_size]);
+            std::random_shuffle(&first_digit_pool[0], &first_digit_pool[arr_size]);
+            
+            
+            
+
+            
+            
             int rand_size_min = 1;
             int rand_size_max = 9;
-            int rand_size = time_rand(rand_size_min,rand_size_max);
+            int rand_size = time_rand(rand_size_min,rand_size_max); //Determines number of digits within random number
             std::string rand_str;
             for (int i = 0; i < rand_size;i++){
                 int rand_index_min = 1;
                 int rand_index_max = arr_size;
                 int rand_index = time_rand(rand_index_min,rand_index_max);
-                rand_str += std::to_string(array[rand_index]);
+                rand_str += std::to_string(first_digit_pool[rand_index]);
             }
-            std::cout << rand_str << std::endl;
+            //std::cout << rand_str << std::endl;
             int rand = std::stoi(rand_str);
             return rand;
         }
